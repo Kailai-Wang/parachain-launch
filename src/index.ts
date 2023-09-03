3f50017957d18ebb5225566b41ad732d07202102
Kailai Wang <Kailai.Wang@hotmail.com>
2023-09-03 17:27:10 +0800

    use tty

--- src/index.ts
+++ src/index.ts
@@ -81,11 +81,11 @@ const getChainspec = (image: string, chain: string) => {
   const tmpChainSpec = `${shell.tempdir()}/${chain}-${new Date().toISOString().slice(0, 10)}.json`;
   if (chain.endsWith('.json')) {
     exec(
-      `stdbuf -o0 -e0 docker run -v $(pwd)/${chain}:/${chain} --rm ${image} build-spec --chain=/${chain} --disable-default-bootnode > ${tmpChainSpec}`,
+      `stdbuf -o0 -e0 docker run -t -v $(pwd)/${chain}:/${chain} --rm ${image} build-spec --chain=/${chain} --disable-default-bootnode > ${tmpChainSpec}`,
     );
   } else {
     exec(
-      `stdbuf -o0 -e0 docker run --rm ${image} build-spec --chain=${chain} --disable-default-bootnode > ${tmpChainSpec}`,
+      `stdbuf -o0 -e0 docker run -t --rm ${image} build-spec --chain=${chain} --disable-default-bootnode > ${tmpChainSpec}`,
     );
   }
 
@@ -122,7 +122,7 @@ const exportParachainGenesis = (parachain: Parachain, output: string) => {
 
   const tmpGenesisWasm = `${shell.tempdir()}/genesis-wasm-${new Date().toISOString().slice(0, 10)}`;
   exec(
-    `stdbuf -o0 -e0 docker run -v "${absOutput}":/app --rm ${parachain.image} export-genesis-wasm ${args.join(
+    `stdbuf -o0 -e0 docker run -t -v "${absOutput}":/app --rm ${parachain.image} export-genesis-wasm ${args.join(
       ' ',
     )} > ${tmpGenesisWasm}`,
   );
@@ -132,7 +132,7 @@ const exportParachainGenesis = (parachain: Parachain, output: string) => {
 
   const tmpGenesisState = `${shell.tempdir()}/genesis-state-${new Date().toISOString().slice(0, 10)}`;
   exec(
-    `stdbuf -o0 -e0 docker run -v "${absOutput}":/app --rm ${parachain.image} export-genesis-state ${args.join(
+    `stdbuf -o0 -e0 docker run -t -v "${absOutput}":/app --rm ${parachain.image} export-genesis-state ${args.join(
       ' ',
     )} > ${tmpGenesisState}`,
   );
@@ -241,7 +241,7 @@ const generateRelaychainGenesisFile = (config: Config, path: string, output: str
   fs.writeFileSync(tmpfile, jsonStringify(spec));
 
   exec(
-    `stdbuf -o0 -e0 docker run --rm -v "${tmpfile}":/${config.relaychain.chain}.json ${config.relaychain.image} build-spec --raw --chain=/${config.relaychain.chain}.json --disable-default-bootnode > ${path}`,
+    `stdbuf -o0 -e0 docker run -t --rm -v "${tmpfile}":/${config.relaychain.chain}.json ${config.relaychain.image} build-spec --raw --chain=/${config.relaychain.chain}.json --disable-default-bootnode > ${path}`,
   );
   exec(`cp ${tmpfile} ${output}/rococo-tmp.json`);
   shell.rm(tmpfile);
@@ -274,7 +274,7 @@ const getAddress = (val: string) => {
  * @param image
  */
 const generateNodeKey = (image: string) => {
-  const res = exec(`stdbuf -o0 -e0 docker run --rm ${image} key generate-node-key`);
+  const res = exec(`stdbuf -o0 -e0 docker run -t --rm ${image} key generate-node-key`);
   return {
     key: res.stdout.trim(),
     address: res.stderr.trim(),
